84d4f463e138e06fafa3473ad120b74809bc7e60
Patrick Buchholz <Patrick.Buchholz@tu-dresden.de>
2019-09-26 12:58:48 +1000

    completed task 3 - added new AppBar with custom color and text - created List of Categories and display custom widgets in a listView

--- course/03_category_route/task_03_category_route/lib/category_route.dart
+++ course/03_category_route/task_03_category_route/lib/category_route.dart
@@ -2,11 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+
 import 'package:flutter/material.dart';
-
-// TODO: Check if we need to import anything
-
-// TODO: Define any constants
+import 'package:task_03_category_route/category.dart';
 
 /// Category Route (screen).
 ///
@@ -40,17 +38,53 @@ class CategoryRoute extends StatelessWidget {
     Colors.red,
   ];
 
+  static const _iconData = Icons.cake;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: Create a list of the eight Categories, using the names and colors
+    // Create a list of the eight Categories, using the names and colors
     // from above. Use a placeholder icon, such as `Icons.cake` for each
     // Category. We'll add custom icons later.
 
-    // TODO: Create a list view of the Categories
-    final listView = Container();
+    List<Category> categoryList = List<Category>();
+    //make sure the list have the same amount of elements
+    if(_categoryNames.length != _baseColors.length){
+      return Container(
+        color: Colors.white,
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Text(
+            'Ooops, something went wrong!',
+            style: TextStyle(color: Colors.black, fontSize: 40.0),
+          )
+        ),
+      );
+    }
+    int index = 0;
+    for (var name in _categoryNames) {
+      categoryList.add(Category(name: name, color: _baseColors[index], iconLocation: _iconData));
+      index += 1;
+    }
 
-    // TODO: Create an App Bar
-    final appBar = AppBar();
+    // Create a list view of the Categories
+    final listView = Container(
+      color: Colors.green[100],
+      //adding horizontal padding the left and right
+      padding: EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+      child: ListView(
+        children: categoryList,
+      ),
+    );
+
+    final appBar = AppBar(
+      elevation: 0.0,
+      backgroundColor: Colors.green[100],
+      centerTitle: true,
+      title: Text(
+        'Unit Converter',
+        style: TextStyle(color: Colors.black, fontSize: 30.0),
+      ),
+    );
 
     return Scaffold(
       appBar: appBar,
